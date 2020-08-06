35127ea64ac8854409b8898f142e27cf6a75a5ab
siyue.huang <siyue.huang@medbanks.cn>
2020-08-06 10:41:38 +0800

    upd: 更新依赖

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Normal from './normal';
-import Uform from './uform';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Normal from "./normal";
 
 function App() {
   return (
     <Router>
       <div style={{ marginBottom: 30 }}>
-        <Link to='/normal' style={{ marginRight: 20 }}>
+        <Link to="/normal" style={{ marginRight: 20 }}>
           normal
         </Link>
-        <Link to='/uform'>uform</Link>
       </div>
       <Switch>
-        <Route path='/normal' component={Normal} />
-        <Route path='/uform' component={Uform} />
+        <Route path="/normal" component={Normal} />
       </Switch>
     </Router>
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
